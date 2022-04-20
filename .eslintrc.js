c4223806cec5f00773e096d0f2243271540bb654
Crmal <pjm2207@naver.com>
2022-04-21 00:51:30 +0900

    setting: eslintrc설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'airbnb-base'],
   root: true,
   env: {
     node: true,
@@ -21,5 +18,55 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+    'camelcase': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'class-methods-use-this': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-constant-condition': ['error', { checkLoops: true }],
+    'no-empty-function': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForOfStatement',
+        message:
+          'iterators/generators require regenerator-runtime, which is too heavyweight for this guide to allow them. Separately, loops should be avoided in favor of array iterations.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message:
+          'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message:
+          '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+    'no-use-before-define': ['error', { functions: false }],
+    'no-useless-constructor': 'off',
+    'no-unused-expressions': ['error', { allowTaggedTemplates: true }],
+    'prefer-const': ['error', { destructuring: 'all' }],
+    'prefer-destructuring': [
+      'error',
+      { array: true, object: true },
+      { enforceForRenamedProperties: true },
+    ],
+    'spaced-comment': ['error', 'always'],
+    'import/extensions': ['error', 'always'],
+    'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
+    'import/no-unresolved': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: false,
+        },
+      },
+    ],
+    'import/prefer-default-export': 'off',
   },
 };
