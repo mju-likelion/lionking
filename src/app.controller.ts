52b6017a744a6e74e77eb2683907f7ccc1f1ea11
Crmal <pjm2207@naver.com>
2022-04-26 01:55:07 +0900

    feat: add Swagger Tags,Operation example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('test API')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'test API', description: 'test 코드입니다.' })
   getHello(): string {
     return this.appService.getHello();
   }
