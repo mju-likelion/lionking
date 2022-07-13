5457203b5cea5124672f47e12136ff2fb98df923
minjung <ypd06021@naver.com>
2022-07-13 21:19:21 +0900

    fix: yarn 에러 수정2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,24 +7,5 @@ cd /home/ec2-user/liontown
 yarn install
 yarn start
 
-# nginx 연결
-
-# echo "> 현재 구동중인 애플리케이션 pid 확인"
-# CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
-
-# if [ -z $CURRENT_PID ]; then
-#     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-# else
-#     echo "> kill -15 $CURRENT_PID"
-#     kill -15 $CURRENT_PID
-#     sleep 5
-# fi
-
-# echo "> 새 어플리케이션 배포"
-# JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-# echo "> $JAR_NAME에 실행권한 추가"
-# chmod +x $JAR_NAME
-
-# echo "> $JAR_NAME 실행"
-# nohup java -jar $JAR_NAME > $REPOSITORY1/nohup.out 2>&1 &
+# nginx
+# pm2 
