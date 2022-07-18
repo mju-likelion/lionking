a97bb1210cea905e8c3a4ba5c9795fcb93152b73
ndaemy <yuyaebean@gmail.com>
2022-07-19 03:16:39 +0900

    feat: port를 환경변수로 지정할 수 있게

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   app.enableCors();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
