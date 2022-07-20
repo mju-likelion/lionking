0cf915db2b29582c5f4fd064c1ccc21ad44583f0
minjung <ypd06021@naver.com>
2022-07-21 02:04:04 +0900

    feat: api 구조

--- src/lounge/lounge.controller.ts
+++ src/lounge/lounge.controller.ts
@@ -1,15 +1,46 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { LoungeService } from './lounge.service';
 
 @UseGuards(AuthGuard())
-@Controller('api/lounges')
+@Controller('api/lounge')
 export class LoungeController {
   constructor(private readonly loungeService: LoungeService) {}
 
+  // 라운지 테스트
   @Get()
   async testLounge() {
     return this.loungeService.testLounge();
+  }
+
+  // 라운지 정보 전체
+  @Get()
+  async Lounges() {
+    return this.loungeService.Lounges();
+  }
+
+  // 라운지 정보 단일
+  @Get('/:id')
+  async Lounge() {
+    return this.loungeService.Lounge();
+  }
+
+  // 라운지 생성
+  @Post()
+  async postLounge() {
+    return this.loungeService.postLounge();
+  }
+
+  // 라운지 수정
+  @Put()
+  async putLounge() {
+    return this.loungeService.putLounge();
+  }
+
+  // 라운지 삭제
+  @Delete()
+  async deleteLounge() {
+    return this.loungeService.deleteLounge();
   }
 }
--- src/lounge/lounge.service.ts
+++ src/lounge/lounge.service.ts
@@ -5,4 +5,24 @@ export class LoungeService {
   async testLounge() {
     return 'testLounge';
   }
+
+  async Lounges() {
+    return 'testLounge';
+  }
+
+  async Lounge() {
+    return 'testLounge';
+  }
+
+  async postLounge() {
+    return 'testLounge';
+  }
+
+  async putLounge() {
+    return 'testLounge';
+  }
+
+  async deleteLounge() {
+    return 'testLounge';
+  }
 }
