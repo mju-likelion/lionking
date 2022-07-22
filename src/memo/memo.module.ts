5e0048deaf86ec5286d8453ced7478a11fe2f9a0
ndaemy <yuyaebean@gmail.com>
2022-07-22 17:01:56 +0900

    fix: TypeORM 이슈 픽스

--- src/memo/memo.module.ts
+++ src/memo/memo.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
 
 import { MemoController } from './memo.controller';
+import { Memo } from './memo.entity';
 import { MemoService } from './memo.service';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forFeature([Memo])],
   controllers: [MemoController],
   providers: [MemoService],
 })
