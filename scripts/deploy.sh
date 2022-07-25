f3f010cd59f56a159003170353db91d8fb1548ff
minjung <ypd06021@naver.com>
2022-07-26 02:08:57 +0900

    fix: 쉘스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,6 @@ REPOSITORY1=/home/ec2-user
 REPOSITORY=/home/ec2-user/liontown
 PROJECT_NAME=liontown
 
-ssh -i lionking-lightsail.pem ubuntu@api.liontown.city
 cd lionking
 git pull
 sudo docker compose down
