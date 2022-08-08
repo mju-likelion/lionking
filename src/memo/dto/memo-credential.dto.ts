bbc45e193ef7bfb9051de62f4fe8d99732b812de
Crmal <pjm2207@naver.com>
2022-08-09 05:27:14 +0900

    feat: MemoCredentialDto ApiProperty 추가

--- src/memo/dto/memo-credential.dto.ts
+++ src/memo/dto/memo-credential.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength } from 'class-validator';
 
 export class MemoCredentialDto {
+  @ApiProperty({ description: '제목', example: '제목1' })
   @IsString()
   @MaxLength(20)
   title?: string;
 
+  @ApiProperty({ description: '내용', example: '내용1' })
   @IsString()
   @MaxLength(1000)
   content?: string;
