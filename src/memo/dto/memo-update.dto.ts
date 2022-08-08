9620678f4a4bcbb3423640058af6ca866268ee52
Crmal <pjm2207@naver.com>
2022-08-09 05:27:46 +0900

    feat: memoUpdateDto (title, content) Optional 적용

--- src/memo/dto/memo-update.dto.ts
+++ src/memo/dto/memo-update.dto.ts
@@ -1,11 +1,13 @@
-import { IsString, MaxLength } from 'class-validator';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class MemoUpdateDto {
   @IsString()
+  @IsOptional()
   @MaxLength(20)
   title?: string;
 
   @IsString()
+  @IsOptional()
   @MaxLength(1000)
   content?: string;
 }
