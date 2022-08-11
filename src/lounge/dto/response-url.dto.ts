61263e19c16d22068e98731d3ce3a1b7c76bda85
Crmal <pjm2207@naver.com>
2022-08-12 01:46:22 +0900

    fix: lounge post swagger 수정

--- src/lounge/dto/response-url.dto.ts
+++ src/lounge/dto/response-url.dto.ts
@@ -3,12 +3,12 @@ import { ApiProperty } from '@nestjs/swagger';
 import { UrlDto } from './url.dto';
 
 export class ResponseUrlDto {
-  constructor(url: string) {
-    this.data = { url };
+  constructor(loungeLink: string) {
+    this.data = { loungeLink };
   }
 
   @ApiProperty({ type: UrlDto })
   data: {
-    url: string;
+    loungeLink: string;
   };
 }
--- src/lounge/dto/url.dto.ts
+++ src/lounge/dto/url.dto.ts
@@ -2,5 +2,5 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class UrlDto {
   @ApiProperty({ description: 'url주소', example: 'https://liontown.city' })
-  url: string;
+  loungeLink: string;
 }
